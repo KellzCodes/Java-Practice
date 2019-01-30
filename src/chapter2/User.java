bec35bffa3bed07da2fb692f37864368e1611594
Kelli Davis <keldavis5@yahoo.com>
2019-01-29 23:47:22 -0500

    remove solutions. I want to do it on my own

--- src/chapter2/User.java
+++
@@ -1,8 +0,0 @@
-package chapter2;
-
-public class User {
-	private String email;
-	private String encryptedPassword;
-	private String firstName;
-	
-}
--- src/chapter2/UserDao.java
+++
@@ -1,5 +0,0 @@
-package chapter2;
-
-public interface UserDao {
-	public User findUserByEmail(String email);
-}
