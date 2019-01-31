4a3027c8d3ebaf3b78e3bb2468a4d3355d2ec017
Kelli Davis <keldavis5@yahoo.com>
2019-01-30 20:00:16 -0500

    Add main class to run the project

---
+++ src/Linked Lists/src/insertNode/LinkedListDemo.java
@@ -0,0 +1,16 @@
+package insertNode;
+
+public class LinkedListDemo {
+    public static void main (String[] args){
+        // Create new linked list
+        LinkedList list = new LinkedList();
+
+        /* Call the insertAtHead method on this List object, and add some values */
+        list.insertAtHead(5);
+        list.insertAtHead(10);
+        list.insertAtHead(2);
+        list.insertAtHead(12);
+        list.insertAtHead(19);
+        list.insertAtHead(20);
+    }
+}
