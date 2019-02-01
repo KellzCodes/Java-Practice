a116ac644d3c051abeecf6d4f246fb8607ae148a
Kelli Davis <keldavis5@yahoo.com>
2019-02-01 01:55:23 -0500

    refactor: rewrite insert at tail method so nodes actually insert at tail

--- src/Linked Lists/src/package1/DoublyEndedList.java
+++ src/Linked Lists/src/package1/DoublyEndedList.java
@@ -8,36 +8,56 @@ package package1;
 * That is the head and the tail.
 * */
 public class DoublyEndedList {
-    // declare two member variables, head and tail
     private Node head;
-    private Node tail;
 
     public void insertAtTail (int data){
         Node newNode = new Node(data);
+        Node last = head;
 
-        // If the head is equal to null, the newNode becomes the head.
+        // New node will be the last node, so it should point to null
+        newNode.nextNode = null;
+
+        // If the head is equal to null (or empty), the newNode becomes the head.
         if (this.head == null){
+            newNode.previousNode = null;
             this.head = newNode;
+            return;
         }
 
-        // If the tail is not equal to null. The newNode becomes the current tail's NextNode
-        if (this.tail != null){
-            this.tail.setNextNode(newNode);
-            this.tail = newNode;
+        // Go through until the last node
+        while(last.nextNode != null){
+            last = last.nextNode;
         }
+
+        // Make the node after the next node the new node
+        last.nextNode = newNode;
+
+        // Make the last node the new node's previous node
+        newNode.previousNode = last;
     }
 
+    /* Print the data for all the nodes contained in the linked list,
+    starting from the head element. */
     @Override
     public String toString(){
         String result = "{";
+        // Declare a variable called current, which points to the head
         Node current = this.head;
 
+        /* Go through all the nodes of the list, until the current node
+        points to a null.*/
         while (current != null){
+            /* get the string created from the Node object and append it
+            to the result variable and separate them by using a comma. */
             result += current.toString() + ", ";
+            // go to the next node
             current = current.getNextNode();
         }
+
+        // Return the result
         result += "}";
         return result;
+
     }
 
 }
