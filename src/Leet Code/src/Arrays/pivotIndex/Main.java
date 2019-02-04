458fa58d5d9ffcb15c23fc3b22c163c57fa54ca0
Kelli Davis <keldavis5@yahoo.com>
2019-02-04 01:19:26 -0500

    refactor: update algorithm for efficiency

--- src/Leet Code/src/Arrays/pivotIndex/Main.java
+++ src/Leet Code/src/Arrays/pivotIndex/Main.java
@@ -1,4 +1,4 @@
-package Arrays.pkg1.pivotIndex;
+package Arrays.pivotIndex;
 
 /*
 * Given an array of integers "nums", write a method that returns the "pivot" index of this array.
@@ -10,34 +10,35 @@ package Arrays.pkg1.pivotIndex;
 public class Main {
     public static void main (String[] args){
         int[] nums = {1, 7, 3, 6, 5, 6};
-        System.out.println(Solution1(nums));
+        System.out.println(pivotIndex(nums));
 
         int[] nums2 = {1, 2, 3};
-        System.out.println(Solution1(nums2));
+        System.out.println(pivotIndex(nums2));
     }
 
-    public static int Solution1(int[] nums){
-        /*
-        * We need to quickly compute the sum of values to the left and
-        * right of every index.
-        * */
-
-        // Sum of numbers
+    public static int pivotIndex(int[] nums) {
+        // The sum of all the numbers in the array
         int sum = 0;
-        // Sum of numbers to the left of the index
-        int leftSum = 0;
 
-        // Add all the numbers in nums array
-        for(int x: nums){
+        // Add all the numbers in nums
+        for(int x : nums) {
             sum += x;
         }
 
-        for(int i = 0; i < nums.length; ++i){
-            // find the right sum
-            if(leftSum == sum - leftSum - nums[i]){
+        // The left side of the array
+        int leftSum = 0;
+
+        // go through the entire array and check if the left side equals the right side
+        for (int i = 0; i < nums.length; i++) {
+            // the right sum is the sum minus the left sum minus the index
+            // if the left side of the current index is equal to the right side, return it
+            if (leftSum == sum - leftSum - nums[i]) {
                 return i;
             }
-            leftSum += nums[i];
+            // if not, add the number at the current index to the left sum
+            else {
+                leftSum += nums[i];
+            }
         }
         return -1;
     }
