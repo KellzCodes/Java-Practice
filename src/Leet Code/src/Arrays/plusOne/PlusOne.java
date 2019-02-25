4a4435e39f35f748cc9809fbc8781ac6ee32d99a
Kelli Davis <keldavis5@yahoo.com>
2019-02-25 13:26:52 -0500

    docs: Update comments and variables for readability

--- src/Leet Code/src/Arrays/plusOne/PlusOne.java
+++ src/Leet Code/src/Arrays/plusOne/PlusOne.java
@@ -18,36 +18,37 @@ Explanation: The array represents the integer 4321.
 
 public class PlusOne {
     public static void main(String[] args){
-        int[] digits = new int[]{9, 9, 9, 9};
+        int[] digits = new int[]{9, 9};
         int[] newDigits = plusOne(digits);
         for (int i = 0; i < newDigits.length; i++){
             System.out.print(newDigits[i] + " ");
         }
     }
     public static int[] plusOne(int[] digits) {
-
         // find out how long the array is
-        int n = digits.length;
+        int arrayLength = digits.length;
 
         // Traverse through the array backward
-        for(int i = n - 1; i >= 0; i--) {
-            // if the last digit is less than 9, add 1 to it then return the array
-            if(digits[i] < 9) {
-                digits[i]++;
+        for(int currentIndex = arrayLength - 1; currentIndex >= 0; currentIndex--) {
+            // if the digit at the current index is less than 9, add 1 to it then return the array
+            if(digits[currentIndex] < 9) {
+                digits[currentIndex]++;
                 return digits;
             }
 
-            // Otherwise, change the digit at the index to zero
-            digits[i] = 0;
+            // Otherwise, change the digit at the current index to zero
+            digits[currentIndex] = 0;
         }
 
         /* If all the numbers are 9 or greater, create a new array with all the digits
-        * plus space for one more digit */
-        int[] newNumber = new int [n + 1];
+        * plus space for one more digit
+        * array.length will return all zeros by default */
+        int[] newDigits = new int [arrayLength + 1];
+
         // Make the digit at the first index 1
-        newNumber[0] = 1;
+        newDigits[0] = 1;
 
         // return the new array
-        return newNumber;
+        return newDigits;
     }
 }
