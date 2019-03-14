0959dc4aa4154dd354bfca820ffeed0efb82c3ba
Kelli Davis <keldavis5@yahoo.com>
2019-03-13 22:48:12 -0400

    docs: add class method lesson

--- src/CS205/src/Week1/objectsAndClasses/Static.java
+++ src/CS205/src/Week1/objectsAndClasses/Static.java
@@ -8,6 +8,28 @@ package Week1.objectsAndClasses;
 * */
 
 public class Static {
+    /*
+    * Methods can also be declared as static and in this case,
+    * rather than being called an instance method, the method
+    * is called a class method. Class methods belong to the class
+    * as well and are not part of objects of the class.
+    *
+    * Every Java application must have a main() method which is a class
+    * method:
+    * public class Main() { public static void main(String[] args) { ... } }
+    *
+    * The java.lang.Math class has several static methods for performing
+    * mathematical operations:
+    * static double Math.abs(double a) absolute value of a static double
+    * Math.log(double a) log base e of a static double
+    * Math.log10(double a) log base 10 of a static double
+    * Math.max(double a, double b) maximum of a and b static double
+    * Math.min(double a, double b) minimum of a and b static double
+    * Math.pow(double a, double b) returns a to the bth power static int
+    * Math.round(double a) returns a rounded up or down static double
+    * Math.sqrt(double a) returns square root of a
+    *
+    * */
     public static void main(String[] args){
         Static obj1 = new Static();
         Static obj2 = new Static();
