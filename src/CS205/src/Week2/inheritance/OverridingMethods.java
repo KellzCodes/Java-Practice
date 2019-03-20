2e36feaa2e9ab5cfb12dbe8de41028773548a147
Kelli Davis <keldavis5@yahoo.com>
2019-03-19 22:37:20 -0400

    feat: Add calling an overriden superclass method

--- src/CS205/src/Week2/inheritance/OverridingMethods.java
+++ src/CS205/src/Week2/inheritance/OverridingMethods.java
@@ -30,4 +30,39 @@ public class OverridingMethods {
     // the superclass. If the behavior of the method in superclass already does
     // exactly what the subclass needs, then the subclass would not override the
     // method
+
+    /*
+    * Inheritance :: Calling an Overridden Superclass Method
+    * Sometimes the behavior of a superclass-inherited method is exactly what the
+    * subclass requires, but the subclass also has to perform some additional operations.
+    *
+    * Example: suppose at a university all students—undergraduate and graduate—are
+    * assessed tuition using the same formula: tuition = $675 × credit hours. However,
+    * graduate students in certain professional programs (e.g., dentistry, pharmacy,
+    * medicine) are assessed an addition professional program fee of $3,500.
+    *
+    * We can write the calcTuition() method in Student that calculates a student's
+    * tuition by multiplying $675 by the number of credit hours. We need to call
+    * that method in the GraduateStudent class to calculate the tuition and then
+    * add the professional program fee when necessary.
+    *
+    * public class Student {
+    *   public double calcTuition() {
+    *       return 675 * creditHours();
+    *   }
+    * }
+    *
+    * public class GraduateStudent extends Student {
+    *   // calcTuition() is overridden and needs to call the superclass (Student)
+    *   // calcTuition() method. Since these two methods have the same name, how do
+    *   // we specify to the compiler that we are calling calcTuition() in Student?
+    *   // By using the super reserved word.
+    *   public double calcTuition() {
+    *       double tuition = super.calcTuition();  // Calls Student.calcTuition();
+    *       if (mProfessionalProgram == true) {
+    *           tuition += 3500;
+    *       } return tuition;
+    *   }
+    * }
+    * */
 }
