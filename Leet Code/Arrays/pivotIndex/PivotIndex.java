28c49404b8b4cf4024a547ec8309c8e9c423a3c2
Kelli Davis <keldavis5@yahoo.com>
2019-05-07 13:03:59 -0400

    docs: update comments

--- Leet Code/Arrays/pivotIndex/PivotIndex.java
+++ Leet Code/Arrays/pivotIndex/PivotIndex.java
@@ -46,7 +46,9 @@ public class PivotIndex {
 
         // Go through the array, starting at index 1
         for (int i = 1; i < nums.length; i++) {
-
+            /* If the number at the last index minus the number at the current index
+            * is equal to the previous index, return  the current index
+            * */
             if (nums[last] - nums[i] == nums[i - 1]) {
                 return i;
             }
