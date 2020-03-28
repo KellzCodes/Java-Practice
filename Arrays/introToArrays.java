4f6443b2cf517c9b6710ba270ef58547a9079a65
Kelli Davis <keldavis5@yahoo.com>
2020-03-27 20:06:49 -0400

    Introduction to Arrays

---
+++ Arrays/introToArrays.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+/*
+* An array is a basic data structure to store a collection of elements sequentially.
+* But elements can be accessed randomly since each element in the array can be
+* identified by an array index.
+* */
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. Initialize
+        int[] a0 = new int[5];
+        int[] a1 = {1, 2, 3};
+        // 2. Get Length
+        System.out.println("The size of a1 is: " + a1.length);
+        // 3. Access Element
+        System.out.println("The first element is: " + a1[0]);
+        // 4. Iterate all Elements
+        System.out.print("[Version 1] The contents of a1 are:");
+        for (int i = 0; i < a1.length; ++i) {
+            System.out.print(" " + a1[i]);
+        }
+        System.out.println();
+        System.out.print("[Version 2] The contents of a1 are:");
+        for (int item: a1) {
+            System.out.print(" " + item);
+        }
+        System.out.println();
+        // 5. Modify Element
+        a1[0] = 4;
+        // 6. Sort
+        Arrays.sort(a1);
+    }
+}
