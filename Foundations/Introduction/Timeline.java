54936713baecd15707a06be26c834352e9ab5285
Kelli Davis <keldavis5@yahoo.com>
2020-04-16 00:30:29 -0400

    feat: add a program that demonstrates the use of comments

---
+++ Foundations/Introduction/Timeline.java
@@ -0,0 +1,18 @@
+public class Timeline {
+  public static void main(String[] args) {
+    System.out.println("Hello Java!");
+    
+    System.out.println("You were born in 1995");
+
+   // Sun Microsystems announced the release of Java in 1995
+    
+    System.out.println("You were created by James Gosling");
+    
+    /*
+		James Gosling is a Canadian engineer who 
+		created Java while working at Sun Microsystems. 
+		His favorite number is the square root of 2!
+    */
+    System.out.println("You are a fun language!");
+  }
+} 
