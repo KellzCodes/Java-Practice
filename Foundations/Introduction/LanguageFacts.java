1b71838822e4e989b7f612da2ba7a3851ad6839d
Kelli Davis <keldavis5@yahoo.com>
2020-04-16 00:51:44 -0400

    add a program that demonstrates the use of white space

---
+++ Foundations/Introduction/LanguageFacts.java
@@ -0,0 +1,14 @@
+public class LanguageFacts {
+  public static void main(String[] args) {
+    // Press enter or return on your keyboard after each semicolon!
+    
+    System.out.println("Java is a class-based language.");
+
+    System.out.println("Java classes have a 'main' method.");
+    
+    System.out.println("Java statements end with a semicolon.");
+
+    System.out.println("Programming is so much fun!");
+  }
+
+}
