86f716d4ee8a8d5509a0b0bf29dfac1952d2907e
Kelli Davis <keldavis5@yahoo.com>
2020-04-19 00:42:14 -0400

    feat: add a program that demonstrates printing strings

---
+++ Foundations/Introduction/Tree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args){
+    // Introducing Kelli D.
+    System.out.println("Hey there, I’m Kelli and I’m learning to code in Java");
+    System.out.println("I’m going to plant a tree today!");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  "); 
+  }
+}
