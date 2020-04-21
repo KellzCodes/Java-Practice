df1b5f972988a262d4838570efe8955b33e9c186
Kelli Davis <keldavis5@yahoo.com>
2020-04-20 21:40:55 -0400

    feat: add a program that demonstrates the use of the boolean data type

---
+++ Foundations/Introduction/Booleans.java
@@ -0,0 +1,8 @@
+public class Booleans {
+	public static void main(String[] args) { 
+    // Declare boolean   
+    boolean intsCanHoldDecimals = false;
+    // Print boolean
+    System.out.println(intsCanHoldDecimals);
+	}
+}
