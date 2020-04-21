5b7b13782d3cff9b26f72aa3ae3067d471101951
Kelli Davis <keldavis5@yahoo.com>
2020-04-21 01:41:59 -0400

    feat: add a program that introduces math operators

---
+++ Foundations/2. Variables/GuessingGame.java
@@ -0,0 +1,8 @@
+public class GuessingGame {
+	public static void main(String[] args) {   
+		int mystery1 = 8 + 6;
+    int mystery2 = 8 - 6;
+    // print the variable that holds the value of 2
+    System.out.println(mystery2);
+	}
+}
