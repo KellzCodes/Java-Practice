b84567ffeca84a622f833e9160ee50e6a2152a5b
Kelli Davis <keldavis5@yahoo.com>
2020-04-20 22:55:36 -0400

    feat: add a program that demonstrates the use of primitive and String data types

---
+++ Foundations/Introduction/MyProfile.java
@@ -0,0 +1,14 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+    // declare a variable that holds your name
+    String name = "Kelli";
+    // declare a variable that holds your age
+    int age = 32;
+    // declare a variable that holds desired salary to a precision of two decimal points
+    double desiredSalary = 1000000000.00;
+    // declare a variable that holds your gender as one character
+    char gender = 'f';
+    // declare a variable that holds whether or not you are currently open to job offers
+    boolean lookingForJob = true;
+	}
+}
