cf36ba9af03f3c4813bfe74205634a8bf0ec9e9f
Kelli Davis <keldavis5@yahoo.com>
2020-04-21 02:49:27 -0400

    feat: add a program that demonstrates the use of the modulo operator

---
+++ Foundations/2. Variables/Modulo.java
@@ -0,0 +1,9 @@
+public class Modulo {
+	public static void main(String[] args) {   
+		int students = 26;
+	    // Create a variable that holds the modulo of students and 3
+	    int leftOut = students % 3;
+	    // print leftOut
+	    System.out.println(leftOut);
+	}
+}
