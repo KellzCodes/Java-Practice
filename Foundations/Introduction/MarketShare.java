c26cc3a2d13f28901faed97f139eeca26d002008
Kelli Davis <keldavis5@yahoo.com>
2020-04-20 21:27:57 -0400

    feat: add a program that demonstrates the use of the double data type

---
+++ Foundations/Introduction/MarketShare.java
@@ -0,0 +1,8 @@
+public class MarketShare {
+	public static void main(String[] args) {
+    // declare a double
+    double androidShare = 81.7;
+    // print the double
+    System.out.println(androidShare);
+	}
+}
