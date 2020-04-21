4d0effb3ef32f2cfc64074e5333ed8972549fb1c
Kelli Davis <keldavis5@yahoo.com>
2020-04-20 21:49:07 -0400

    feat: add a program that demonstrates the use of the char data type

---
+++ Foundations/Introduction/Char.java
@@ -0,0 +1,8 @@
+public class Char {
+	public static void main(String[] args) {   
+    // Declare a char
+    char expectedGrade = 'A';
+    // print a char
+    System.out.println(expectedGrade);
+	}
+}
