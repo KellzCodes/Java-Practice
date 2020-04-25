997dba9d0bbd2c879cfe0856f270f1fc26434081
Kelli Davis <keldavis5@yahoo.com>
2020-04-25 01:46:06 -0400

    feat: add a Dog class

---
+++ Foundations/3. Classes and Objects/Dog.java
@@ -0,0 +1,24 @@
+public class Dog {
+  String breed;
+  boolean hasOwner;
+  int age;
+  
+  public Dog(String dogBreed, boolean dogOwned, int dogYears) {
+    System.out.println("Constructor invoked!");
+    breed = dogBreed;
+    hasOwner = dogOwned;
+    age = dogYears;
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("Main method started");
+    Dog fido = new Dog("poodle", false, 4);
+    Dog nunzio = new Dog("shiba inu", true, 12);
+    boolean isFidoOlder = fido.age > nunzio.age;
+    int totalDogYears = nunzio.age + fido.age;
+    System.out.println("Two dogs created: a " + fido.breed + " and a " + nunzio.breed);
+    System.out.println("The statement that fido is an older dog is: " + isFidoOlder);
+    System.out.println("The total age of the dogs is: " + totalDogYears);
+    System.out.println("Main method finished");
+  }
+}
