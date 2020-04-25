d8aa5f12403fe4e1cc8a3c2e02a7d83e7bfc5376
Kelli Davis <keldavis5@yahoo.com>
2020-04-24 21:54:32 -0400

    feat: add program that demonstrates the use of .equals()

---
+++ Foundations/2. Variables/Sing.java
@@ -0,0 +1,11 @@
+public class Sing {
+  	public static void main(String[] args){
+      String line1 = "Nah nah nah nah nah nah nah nah nah yeah";
+      String line2 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+      String line3 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+      // print out whether line1 and line2 are the same.
+      System.out.println(line1.equals(line2));
+      // print whether line2 and line3 are equal
+      System.out.println(line2.equals(line3));
+    }       
+}
