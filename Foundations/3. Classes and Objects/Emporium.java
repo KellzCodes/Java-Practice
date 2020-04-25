375e968371f9bbd8f2ad58935f0627ada6bf5be7
Kelli Davis <keldavis5@yahoo.com>
2020-04-25 00:56:50 -0400

    feat: add a program that demonstrates constructor parameters

---
+++ Foundations/3. Classes and Objects/Emporium.java
@@ -0,0 +1,15 @@
+public class Emporium {
+  // instance fields
+  String productType;
+  
+  // constructor method
+  public Emporium(String product) {
+    productType = product;
+  }
+  
+  // main method
+  public static void main(String[] args) {
+    
+    
+  }
+}
