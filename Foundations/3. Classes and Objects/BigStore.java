d936d13f50a8283a453f4bc616251eb131ca4c29
Kelli Davis <keldavis5@yahoo.com>
2020-04-26 02:08:58 -0400

    feat: add program that demonstrates adding parameters to methods

---
+++ Foundations/3. Classes and Objects/BigStore.java
@@ -0,0 +1,25 @@
+public class BigStore {
+  // instance fields
+  String productType;
+  
+  // constructor method
+  public BigStore(String product) {
+    productType = product;
+  }
+  
+  // advertise method
+  public void advertise() {
+    String message = "Selling " + productType + "!";
+		System.out.println(message);
+  }
+  
+  public void greetCustomer(String customer){
+    System.out.println("Welcome to the BigStore, " + customer + "!");
+  }
+  
+  // main method
+  public static void main(String[] args) {
+    BigStore lemonadeStand = new BigStore("Lemonade");
+    lemonadeStand.greetCustomer("Hello World");
+  }
+}
