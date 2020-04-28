badea16d23b5bf736907ccd7a57abb07abf4d733
Kelli Davis <keldavis5@yahoo.com>
2020-04-27 21:13:41 -0400

    feat: add checking account program

---
+++ Foundations/3. Classes and Objects/CheckingAccount.java
@@ -0,0 +1,55 @@
+public class CheckingAccount {
+  
+  int balance;
+  
+  public CheckingAccount(int initialBalance){
+    balance = initialBalance;
+  }
+  
+  public void checkBalance(){
+    System.out.println("Hello!");
+    System.out.println("Your balance is "+ balance);
+  }
+  
+  public void deposit(int amountToDeposit){
+    balance = amountToDeposit + balance;
+    System.out.println("You just deposited " + amountToDeposit);
+  }
+  
+  public int withdraw(int amountToWithdraw){
+    balance = balance - amountToWithdraw;
+    System.out.println("You just withdrew " + amountToWithdraw);
+    return amountToWithdraw;
+  }
+  
+  public String toString(){
+    return "This is a checking account with " + balance + " saved.";
+  }
+  
+  public static void main(String[] args){
+    CheckingAccount checking = new CheckingAccount(2000);
+    
+    //Check balance:
+    checking.checkBalance();
+    
+    //Withdrawing:
+    checking.withdraw(300);
+    
+    //Check balance:
+    checking.checkBalance();
+    
+    //Deposit:
+    checking.deposit(600);
+    
+    //Check balance:
+    checking.checkBalance();
+    
+    //Deposit:
+    checking.deposit(600);
+    
+    //Check balance:
+    checking.checkBalance();
+    
+    System.out.println(checking);
+  }       
+}
