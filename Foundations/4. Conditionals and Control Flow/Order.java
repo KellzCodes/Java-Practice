3b62ab0ce27f050002c5b782d03a80c57542f41c
Kelli Davis <keldavis5@yahoo.com>
2020-04-28 22:46:30 -0400

    feat: add program that shows how to use if then statement

---
+++ Foundations/4. Conditionals and Control Flow/Order.java
@@ -0,0 +1,14 @@
+public class Order {
+  
+  public static void main(String[] args) {
+    
+    double itemCost = 30.99;
+    
+    // Write an if-then statement that prints High value item! when itemCost is greater than 24.00.
+    if(itemCost > 24.00){
+      System.out.println("High value item!");
+    }
+    
+  }
+  
+}
