fbec1a9790d0f017cd47fc89dc0f3b64e6746123
Kelli Davis <keldavis5@yahoo.com>
2020-04-28 23:40:47 -0400

    feat: add program that Shows how to use if-then-else-if statements

---
+++ Foundations/4. Conditionals and Control Flow/BiggerOrder.java
@@ -0,0 +1,46 @@
+public class BiggerOrder {
+  boolean isFilled;
+  double billAmount;
+  String shipping;
+  
+  public BiggerOrder(boolean filled, double cost, String shippingMethod) {
+		if (cost > 24.00) {
+      System.out.println("High value item!");
+    }
+    isFilled = filled;
+    billAmount = cost;
+    shipping = shippingMethod;
+  }
+  
+  public void ship() {
+    if (isFilled) {
+      System.out.println("Shipping");
+      System.out.println("Shipping cost: " + calculateShipping());
+    } else {
+      System.out.println("Order not ready");
+    }
+  }
+  
+  public double calculateShipping() {
+	 	// declare conditional statement here
+    /*
+    When the shipping instance field equals 
+    "Regular", the method should return 0. 
+    When the shipping instance field equals "Express", 
+    the method should return 1.75. 
+    Else the method should return .50.
+*/
+    if(shipping.equals("Regular")) return 0;
+    else if(shipping.equals("Express")) return 1.75;
+    else return 0.50;
+ 	}
+  
+  public static void main(String[] args) {
+    // do not alter the main method!
+    BiggerOrder book = new BiggerOrder(true, 9.99, "Express");
+    BiggerOrder chemistrySet = new BiggerOrder(false, 72.50, "Regular");
+    
+    book.ship();
+    chemistrySet.ship();
+  }
+}
