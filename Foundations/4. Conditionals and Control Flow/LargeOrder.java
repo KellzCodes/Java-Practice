8b00e26c5706dadee9a6b687092c0e5e0986bd5f
Kelli Davis <keldavis5@yahoo.com>
2020-04-29 00:13:09 -0400

    feat: add program that Shows how to use Switch Statement

---
+++ Foundations/4. Conditionals and Control Flow/LargeOrder.java
@@ -0,0 +1,54 @@
+public class LargeOrder {
+  boolean isFilled;
+  double billAmount;
+  String shipping;
+  
+  public LargeOrder(boolean filled, double cost, String shippingMethod) {
+		if (cost > 24.00) {
+      System.out.println("High value item!");
+    }
+    isFilled = filled;
+    billAmount = cost;
+    shipping = shippingMethod;
+  }
+  
+  public void ship() {
+    if (isFilled) {
+      System.out.println("Shipping");
+      System.out.println("Shipping cost: " + calculateShipping());
+    } else {
+      System.out.println("Order not ready");
+    }
+  }
+  
+  public double calculateShipping() {
+    double shippingCost;
+	 	/*
+    When shipping matches "Regular", shippingCost should be 0.
+    When shipping matches "Express", shippingCost should be 1.75. 
+    The default should assign .50 to shippingCost.
+    */
+    switch (shipping){
+      case "Regular":
+        shippingCost = 0;
+        break;
+      case "Express":
+        shippingCost = 1.75;
+        break;
+      default:
+        shippingCost = 0.50;
+    }
+    
+    
+    return shippingCost;
+ 	}
+  
+  public static void main(String[] args) {
+    // do not alter the main method!
+    LargeOrder book = new LargeOrder(true, 9.99, "Express");
+    LargeOrder chemistrySet = new LargeOrder(false, 72.50, "Regular");
+    
+    book.ship();
+    chemistrySet.ship();
+  }
+}
