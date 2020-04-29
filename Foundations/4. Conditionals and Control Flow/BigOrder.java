d0be5b0001e909c0b24f81f7fcad4b087ac82d68
Kelli Davis <keldavis5@yahoo.com>
2020-04-28 23:12:16 -0400

    feat: add program that shows how to use if-then-else statement

---
+++ Foundations/4. Conditionals and Control Flow/BigOrder.java
@@ -0,0 +1,17 @@
+public class BigOrder {
+  
+  public static void main(String[] args) {
+    
+    boolean isFilled = false;
+    
+    // Write an if-then-else statement:
+    if(isFilled == true){
+      System.out.println("Shipping");
+    }
+    else{
+      System.out.println("Order not ready");
+    }
+    
+  }
+  
+}
