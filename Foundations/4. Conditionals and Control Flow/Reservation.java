fb030bef90169b50818eabe70f9ccda7056be116
Kelli Davis <keldavis5@yahoo.com>
2020-04-29 21:35:56 -0400

    feat: add a program that Shows how to use Conditional && operator

---
+++ Foundations/4. Conditionals and Control Flow/Reservation.java
@@ -0,0 +1,48 @@
+public class Reservation {
+  int guestCount;
+  int restaurantCapacity;
+  boolean isRestaurantOpen;
+  boolean isConfirmed;
+  
+  public Reservation(int count, int capacity, boolean open) {
+    guestCount = count;
+		restaurantCapacity = capacity;
+		isRestaurantOpen = open;
+  }  
+  
+  public void confirmReservation() {
+    /* 
+       Write conditional
+       ~~~~~~~~~~~~~~~~~
+       if restaurantCapacity is greater
+       or equal to guestCount
+       AND
+       the restaurant is open:
+         print "Reservation confirmed"
+         set isConfirmed to true
+       else:
+         print "Reservation denied"
+         set isConfirmed to false
+    */
+    if (restaurantCapacity >= guestCount && isRestaurantOpen == true){
+      System.out.println("Reservation confirmed");
+      isConfirmed = true;
+    }else{
+      System.out.println("Reservation denied");
+      isConfirmed = false;
+    }
+  }
+  
+  public void informUser() {
+    System.out.println("Please enjoy your meal!");
+  }
+  
+  public static void main(String[] args) {
+    Reservation partyOfThree = new Reservation(3, 12, true);
+    Reservation partyOfFour = new Reservation(4, 3, true);
+    partyOfThree.confirmReservation();
+    partyOfThree.informUser();
+    partyOfFour.confirmReservation();
+    partyOfFour.informUser();
+  }
+}
