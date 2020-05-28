27ac53f3988390193fe9c37689805d4cf2ec35e2
Kelli Davis <keldavis5@yahoo.com>
2020-05-28 01:58:30 -0400

    feat: add program that shows how to use all elements of the array

---
+++ Foundations/5. Arrays/Review/Classroom.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Classroom {
+  
+  public static void main(String[] args){
+    String[] students = {"Sade", "Alexus", "Sam", "Koma"};
+    double[] mathScores = new double[4];
+    mathScores[0] = 94.5;
+    mathScores[2] = 76.8;
+    
+    System.out.println("The number of students in the class is " + students.length + ".");
+  }
+}
