cbbd724da0d3f287dfc3c980716a4b3c1feb5d81
Kelli Davis <keldavis5@yahoo.com>
2020-05-28 00:47:04 -0400

    feat: add program that shows how to import arrays

---
+++ Foundations/5. Arrays/Importing Arrays/Newsfeed.java
@@ -0,0 +1,22 @@
+// import the Arrays package here:
+import java.util.Arrays;
+
+public class Newsfeed {
+  
+  
+  public Newsfeed(){
+    
+  }
+    
+  public String[] getTopics(){
+    String[] topics = {"Opinion", "Tech", "Science", "Health"};
+    return topics;
+  }
+  
+
+  public static void main(String[] args){
+    Newsfeed sampleFeed = new Newsfeed();
+    String[] topics = sampleFeed.getTopics();
+    System.out.println(Arrays.toString(topics));
+  }
+}
