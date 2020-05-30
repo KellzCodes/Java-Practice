0bb2d63764713bc4a352cd967436f06325062880
Kelli Davis <keldavis5@yahoo.com>
2020-05-29 21:23:19 -0400

    feat: add a program that demonstrates the need for an ArrayList - ArrayIndexOutOfBounds Exception

---
+++ Foundations/5. Arrays and ArrayLists/Intro to ArrayLists/Shopping.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Shopping {
+  
+  public static void main(String[] args) {
+    
+    String[] groceryItems = {"steak", "milk", "jelly beans"};
+    double[] prices = {25.00, 2.95, 2.50};
+    
+    // Adding ham to the groceries
+    groceryItems[3] = "ham";
+    prices[3] = 4.99;
+    
+  }
+  
+}
