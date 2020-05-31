dbc19abcbc3aebb7f7da994a0dd49096b5bc523a
Kelli Davis <keldavis5@yahoo.com>
2020-05-30 21:34:58 -0400

    feat: add a program that shows how to create an ArrayList

---
+++ Foundations/5. Arrays and ArrayLists/Creating ArrayLists/ToDos.java
@@ -0,0 +1,13 @@
+// import the ArrayList package here:
+import java.util.ArrayList;
+
+class ToDos {
+  
+  public static void main(String[] args) {
+    
+    // Create toDoList below:
+    ArrayList<String> toDoList = new ArrayList<String>();
+    
+  }
+  
+}
