8c08dddc66cba8a7e0dea074763652ae2467e1c6
Kelli Davis <keldavis5@yahoo.com>
2020-05-31 22:22:07 -0400

    feat: add a program that shows how to add an item to an ArrayList

---
+++ Foundations/6. ArrayLists/Adding an Item/ToDos.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class ToDos {
+    
+  public static void main(String[] args) {
+    
+    ArrayList<String> toDoList = new ArrayList<String>();
+    String toDo1 = "Water plants";
+    // Add more to-dos here:
+    String toDo2 = "Do laundry";
+    String toDo3 = "Pet Ham and Ghoti until they purr";
+    
+    // Add to-dos to toDoList
+    toDoList.add(toDo1);
+    toDoList.add(toDo2);
+    toDoList.add(toDo3);
+    
+    System.out.println(toDoList);
+      
+    
+  }
+  
+}
