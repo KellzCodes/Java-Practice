5d4455ccbcbdd7b7ba8111b19813eeef1c4ef3bb
Kelli Davis <keldavis5@yahoo.com>
2020-05-31 22:36:18 -0400

    feat: add a program that shows how to use arraylist size

---
+++ Foundations/6. ArrayLists/ArrayList Size/ToDos.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+class ToDos {
+    
+  public static void main(String[] args) {
+    
+    // Sherlock
+    ArrayList<String> sherlocksToDos = new ArrayList<String>();
+    
+    sherlocksToDos.add("visit the crime scene");
+    sherlocksToDos.add("play violin");
+    sherlocksToDos.add("interview suspects");
+    sherlocksToDos.add("solve the case");
+    sherlocksToDos.add("apprehend the criminal");
+    
+    // Poirot
+    ArrayList<String> poirotsToDos = new ArrayList<String>();
+    
+    poirotsToDos.add("visit the crime scene");
+    poirotsToDos.add("interview suspects");
+    poirotsToDos.add("let the little grey cells do their work");
+    poirotsToDos.add("trim mustache");
+    poirotsToDos.add("call all suspects together");
+    poirotsToDos.add("reveal the truth of the crime");
+    
+    // Print the size of each ArrayList below:
+    System.out.println(sherlocksToDos.size());
+    System.out.println(poirotsToDos.size());
+      
+    // Print the name of the detective with the larger to-do list:
+    System.out.println("Poirot");
+  }
+  
+}
