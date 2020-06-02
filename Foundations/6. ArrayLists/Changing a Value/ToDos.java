1eaa5208528bf14c937b4f9d2a041f0f9bb98be8
Kelli Davis <keldavis5@yahoo.com>
2020-06-01 23:05:25 -0400

    feat: add a program that shows how to change a value in an array list

---
+++ Foundations/6. ArrayLists/Changing a Value/ToDos.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+class ToDos {
+    
+  public static void main(String[] args) {
+    
+    // Sherlock
+    ArrayList<String> sherlocksToDos = new ArrayList<String>();
+    
+    sherlocksToDos.add("visit the crime scene");
+    sherlocksToDos.add("play violin");
+    sherlocksToDos.add("interview suspects");
+    sherlocksToDos.add("solve the case");
+    sherlocksToDos.add("apprehend the criminal");
+    
+    // Poirot
+    ArrayList<String> poirotsToDos = new ArrayList<String>();
+    
+    poirotsToDos.add("visit the crime scene");
+    poirotsToDos.add("interview suspects");
+    poirotsToDos.add("let the little grey cells do their work");
+    poirotsToDos.add("trim mustache");
+    poirotsToDos.add("call all suspects together");
+    poirotsToDos.add("reveal the truth of the crime");
+    
+    // Set each to-do below:
+    sherlocksToDos.set(1, "listen to Dr. Watson for amusement");
+    poirotsToDos.set(3, "listen to Captain Hastings for amusement");
+    
+    System.out.println("Sherlock's to-do list:");
+    System.out.println(sherlocksToDos.toString() + "\n");
+    System.out.println("Poirot's to-do list:");
+    System.out.println(poirotsToDos.toString());
+  }
+  
+}
