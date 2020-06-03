514d646c8c7fd3e84005de6d6b5996fc480fea6c
Kelli Davis <keldavis5@yahoo.com>
2020-06-02 23:19:28 -0400

    feat: add a program that shows how to remove an item from an array list

---
+++ Foundations/6. ArrayLists/Removing an Item/ToDos.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+class ToDos {
+    
+  public static void main(String[] args) {
+    
+    // Sherlock
+    ArrayList<String> sherlocksToDos = new ArrayList<String>();
+    
+    sherlocksToDos.add("visit the crime scene");
+    sherlocksToDos.add("play violin");
+    sherlocksToDos.add("interview suspects");
+    sherlocksToDos.add("solve the case");
+    sherlocksToDos.add("apprehend the criminal");
+    
+    // Poirot
+    ArrayList<String> poirotsToDos = new ArrayList<String>();
+    
+    poirotsToDos.add("visit the crime scene");
+    poirotsToDos.add("interview suspects");
+    poirotsToDos.add("let the little grey cells do their work");
+    poirotsToDos.add("trim mustache");
+    poirotsToDos.add("call all suspects together");
+    poirotsToDos.add("reveal the truth of the crime");
+    
+    // Remove each to-do below:
+    sherlocksToDos.remove("visit the crime scene");
+    poirotsToDos.remove("visit the crime scene");
+    sherlocksToDos.remove("play violin");
+      
+    System.out.println(sherlocksToDos.toString() + "\n");
+    System.out.println(poirotsToDos.toString());
+  }
+  
+}
