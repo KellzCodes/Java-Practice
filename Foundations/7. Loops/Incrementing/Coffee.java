f192e2abf226bd191a5f08ec78350818b079bc40
Kelli Davis <keldavis5@yahoo.com>
2020-06-05 01:42:41 -0400

    feat: add a program that shows how to increment in a loop

---
+++ Foundations/7. Loops/Incrementing/Coffee.java
@@ -0,0 +1,18 @@
+class Coffee {
+  
+  public static void main(String[] args) {
+    
+    // initialize cupsOfCoffee
+    int cupsOfCoffee = 1;
+    
+    // add while loop with counter
+    while (cupsOfCoffee <= 100) {
+      
+      System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+      cupsOfCoffee++;
+      
+    }
+    
+  }
+  
+}
