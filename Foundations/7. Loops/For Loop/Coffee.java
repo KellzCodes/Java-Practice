c84b19493cfad325c7d60ffefd43b931dbc75c23
Kelli Davis <keldavis5@yahoo.com>
2020-06-05 02:17:21 -0400

    feat: add a program that shows how to use a for loop

---
+++ Foundations/7. Loops/For Loop/Coffee.java
@@ -0,0 +1,13 @@
+class Coffee {
+  
+  public static void main(String[] args) {
+    
+    for (int cupsOfCoffee = 1; cupsOfCoffee <= 100; cupsOfCoffee++) {
+      
+      System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+      
+    }
+    
+  }
+  
+}
