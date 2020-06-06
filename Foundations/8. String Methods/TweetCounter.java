5a146e622f58a417305e6b10b916847f09ea280e
Kelli Davis <keldavis5@yahoo.com>
2020-06-06 19:59:18 -0400

    feat: add a program that shows how to use the length string method

---
+++ Foundations/8. String Methods/TweetCounter.java
@@ -0,0 +1,12 @@
+public class TweetCounter {
+  
+	public static void main(String[] args) {
+    
+    String tweet = "Liz Lemon, ninjas are kind of cool... I just dont know any personally. Get on that.";  
+
+    // What's the character count?
+    System.out.println(tweet.length());
+    
+  }
+  
+}
