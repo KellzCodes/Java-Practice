8825046900841be888c8456f4740b639d08cda4a
Kelli Davis <keldavis5@yahoo.com>
2020-06-06 21:24:56 -0400

    feat: add a program that shows how to use the indexOf string method

---
+++ Foundations/8. String Methods/indexOf/MorseCode.java
@@ -0,0 +1,12 @@
+public class MorseCode {
+  
+  public static void main(String[] args) {
+    
+    String code = ".... .. / .. .----. -- / ... --- -. -. -.-- / .- -. -.. / .. / .-.. .. -.- . / - --- / . .- - / .-. .- -- . -.";
+
+    // Write the code:
+    System.out.println(code.indexOf(". .- -"));
+    
+  }
+  
+}
