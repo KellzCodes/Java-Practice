2f5522af4b037178ba751ca4aa4c77e2b44b599d
Kelli Davis <keldavis5@yahoo.com>
2020-06-07 04:17:38 -0400

    feat: add a program that shows a logic error

---
+++ Foundations/10. Debugging/Logic Errors/Debug.java
@@ -0,0 +1,15 @@
+public class Debug {
+
+  public static void main(String[] args) {
+    
+    int steps = 10;
+    
+    for (int i = 1; i <= steps; i++) { // the error was i = 0 on this line
+      
+      System.out.println("Step #" + i);
+      
+    }
+    
+  }
+  
+}
