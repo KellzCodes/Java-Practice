59427efca965d895c199c7ca32b24e0e14cc2867
Kelli Davis <keldavis5@yahoo.com>
2020-06-06 21:36:57 -0400

    feat: add a program that shows how to use charAt string method

---
+++ Foundations/8. String Methods/charAt/Initials.java
@@ -0,0 +1,15 @@
+public class Initials {
+  
+	public static void main(String[] args) {
+    
+    // Add a first name and a last name:
+    String firstName = "Sonny";  
+    String lastName = "Li";
+    
+    // What are the initials?
+    System.out.println(firstName.charAt(0));
+    System.out.println(lastName.charAt(0));
+    
+  }
+  
+}
