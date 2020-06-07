a7ad6583975255eb114fbe9fb3a4bbe7d5b1ea44
Kelli Davis <keldavis5@yahoo.com>
2020-06-06 22:09:39 -0400

    feat: add a program that shows how to use the toLowerCase string method

---
+++ Foundations/8. String Methods/toUpperCase and toLowerCase/Hashtag.java
@@ -0,0 +1,12 @@
+public class Hashtag {
+  
+  public static void main(String[] args) {
+    
+    String hashtag = "#100DaysOfCode";
+    
+    // Make the hashtag lowercase:
+    System.out.println(hashtag.toLowerCase());
+    
+  }
+  
+}
