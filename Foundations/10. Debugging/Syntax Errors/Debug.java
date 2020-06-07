c2eed17b5ac40ab28c70b6a5b48085fcde182be7
Kelli Davis <keldavis5@yahoo.com>
2020-06-07 03:42:06 -0400

    feat: add program that shows a syntax error

---
+++ Foundations/10. Debugging/Syntax Errors/Debug.java
@@ -0,0 +1,12 @@
+public class Debug {
+
+  public static void main(String[] args) {
+    
+    System.out.println("       1");
+    System.out.println("     2 3");
+    System.out.println("   4 5 6");
+    System.out.println("7 8 9 10"); // the bug was a missing semi-colon on this line
+    
+  }
+  
+}
