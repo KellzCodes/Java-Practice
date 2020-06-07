eb5d9394b30873a310076fc73ab54ab09ac369ab
Kelli Davis <keldavis5@yahoo.com>
2020-06-06 20:47:36 -0400

    feat: add a program that shows how to use the concat string method

---
+++ Foundations/8. String Methods/concat/CombineNames.java
@@ -0,0 +1,14 @@
+public class CombineNames {
+  
+	public static void main(String[] args) {
+    
+    // Add your first names:
+    String firstName = "Kelli";  
+    String lastName = "Davis";
+
+    // What's the full name?
+    System.out.println(firstName.concat(" " + lastName));
+    
+  }
+  
+}
