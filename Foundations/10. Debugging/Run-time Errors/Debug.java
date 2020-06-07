61360e87ab7951f2bf7f54e11e4468811ddf5a0e
Kelli Davis <keldavis5@yahoo.com>
2020-06-07 03:54:07 -0400

    feat: add a program that shows run time error

---
+++ Foundations/10. Debugging/Run-time Errors/Debug.java
@@ -0,0 +1,14 @@
+public class Debug {
+
+  public static void main(String[] args) {
+    
+    int width = 20;
+    int length = 40;
+    
+    int ratio = length / width;
+    
+    System.out.println(ratio);
+    
+  }
+  
+}
