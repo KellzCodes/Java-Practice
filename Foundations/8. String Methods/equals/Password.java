1ed5f7d8947fcc556e9d34219a5ab4351010edf6
Kelli Davis <keldavis5@yahoo.com>
2020-06-06 21:09:43 -0400

    feat: add a program that shows how to use the equals string method

---
+++ Foundations/8. String Methods/equals/Password.java
@@ -0,0 +1,12 @@
+public class Password {
+  
+  public static void main(String[] args) {
+    
+    String password = "correcthorsebatterystaple";
+    
+    // Write the code:
+    System.out.println(password.equals("Tr0ub4dor&3"));
+    
+  }
+  
+}
