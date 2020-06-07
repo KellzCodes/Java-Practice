1ae19db7e690457b196b9d35d9162bb9fd91e7c9
Kelli Davis <keldavis5@yahoo.com>
2020-06-06 21:54:25 -0400

    feat: add a program that shows how to use the substring string method

---
+++ Foundations/8. String Methods/substring/Poetry.java
@@ -0,0 +1,11 @@
+public class Poetry {
+  
+	public static void main(String[] args) {
+      
+    String line = "The Heav'ns and all the Constellations rung";     
+    // Change the arguments:
+    System.out.println(line.substring(4, 11));
+    
+  }
+  
+}
