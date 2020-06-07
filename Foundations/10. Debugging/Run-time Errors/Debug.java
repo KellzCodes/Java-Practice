40b9b117637f04ebe0879aef322515089e76b02d
Kelli Davis <keldavis5@yahoo.com>
2020-06-07 03:54:54 -0400

    chore: add comments

--- Foundations/10. Debugging/Run-time Errors/Debug.java
+++ Foundations/10. Debugging/Run-time Errors/Debug.java
@@ -2,10 +2,10 @@ public class Debug {
 
   public static void main(String[] args) {
     
-    int width = 20;
+    int width = 20; // The error was a zero on this line
     int length = 40;
     
-    int ratio = length / width;
+    int ratio = length / width; // A number cannot be divided by zero
     
     System.out.println(ratio);
     
