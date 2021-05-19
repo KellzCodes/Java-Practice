14cfb8c63748e206a4173cf22b284fa60cf66bec
Kelli Davis <keldavis5@yahoo.com>
2021-05-19 18:02:59 -0400

    Create MaxOnesSolution.java

---
+++ Google Interview Prep/Data Structures/arrays/1. Introduction/Max Consecutive Ones/MaxOnesSolution.java
@@ -0,0 +1,18 @@
+class MaxOnesSolution {
+  public int findMaxConsecutiveOnes(int[] nums) {
+    int count = 0;
+    int maxCount = 0;
+    for(int i = 0; i < nums.length; i++) {
+      if(nums[i] == 1) {
+        // Increment the count of 1's by one.
+        count += 1;
+      } else {
+        // Find the maximum till now.
+        maxCount = Math.max(maxCount, count);
+        // Reset count of 1.
+        count = 0;
+      }
+    }
+    return Math.max(maxCount, count);
+  }
+}
