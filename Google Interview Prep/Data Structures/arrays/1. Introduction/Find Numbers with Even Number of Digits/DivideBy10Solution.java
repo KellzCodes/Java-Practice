482bcd76ff9b9111ec05c5757d0c8b9cde1e0914
Kelli Davis <keldavis5@yahoo.com>
2021-05-23 02:57:25 -0400

    Update DivideBy10Solution.java

--- Google Interview Prep/Data Structures/arrays/1. Introduction/Find Numbers with Even Number of Digits/DivideBy10Solution.java
+++ Google Interview Prep/Data Structures/arrays/1. Introduction/Find Numbers with Even Number of Digits/DivideBy10Solution.java
@@ -1,18 +1,17 @@
 class DivideBy10Solution {
     public int findNumbers(int[] nums) {
         int count = 0;
-        int digit = 0;
-        for (int n: nums){
-            while(n > 0){
-                n /= 10;
+        for (int n : nums) {
+            int digit = 0;
+            int number = n;
+            while (number > 0) {
+                number /= 10;
                 digit++;
             }
-            
-            if(digit % 2 == 0){
+            if(digit % 2 == 0) {
                 count++;
             }
         }
-        
         return count;
     }
 }
