d75801093228ac7eae7cc387bbe6530cc328106b
Kelli Davis <keldavis5@yahoo.com>
2021-05-24 00:36:12 -0400

    Create EndThreePointer.java

---
+++ Google Interview Prep/Data Structures/arrays/2. Inserting Items Into an Array/Merge Sorted Array/EndThreePointer.java
@@ -0,0 +1,20 @@
+class EndThreePointer {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // Set p1 and p2 to point to the end of their respective arrays.
+        int p1 = m - 1;
+        int p2 = n - 1;
+        
+        // And move p backwards through the array, each time writing
+        // the smallest value pointed at by p1 or p2.
+        for (int p = m + n - 1; p >= 0; p--) {
+            if (p2 < 0) {
+                break;
+            }
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1--];
+            } else {
+                nums1[p] = nums2[p2--];
+            }
+        }
+    }
+}
