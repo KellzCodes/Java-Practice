efa3deb1c1e63aeaf812dd23f77fe24ef7578435
Kelli Davis <keldavis5@yahoo.com>
2021-05-26 01:10:33 -0400

    Create TwoPointerSolution.java

---
+++ Google Interview Prep/Data Structures/arrays/3. Deleting Items from an Array/Remove Element/TwoPointerSolution.java
@@ -0,0 +1,10 @@
+public int removeElement(int[] nums, int val) {
+    int i = 0;
+    for (int j = 0; j < nums.length; j++) {
+        if (nums[j] != val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+}
