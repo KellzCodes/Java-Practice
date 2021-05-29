6cbc8fdce468be7ae0d643f3e5f4b8df79c3f54d
Kelli Davis <keldavis5@yahoo.com>
2021-05-29 01:02:04 -0400

    Create HeightChecker.java

---
+++ Google Interview Prep/Data Structures/arrays/6. Practice/Height Checker/HeightChecker.java
@@ -0,0 +1,25 @@
+class HeightChecker {
+    public int heightChecker(int[] heights) {
+        int[] heightToFreq = new int[101];
+        
+        for (int height : heights) {
+            heightToFreq[height]++;
+        }
+        
+        int result = 0;
+        int curHeight = 0;
+        
+        for (int i = 0; i < heights.length; i++) {
+            while (heightToFreq[curHeight] == 0) {
+                curHeight++;
+            }
+            
+            if (curHeight != heights[i]) {
+                result++;
+            }
+            heightToFreq[curHeight]--;
+        }
+        
+        return result;
+    }
+}
