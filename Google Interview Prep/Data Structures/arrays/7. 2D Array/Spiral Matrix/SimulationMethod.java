8ee34d3ca72c42111178525bc4f27b173753c3b0
Kelli Davis <keldavis5@yahoo.com>
2021-06-23 20:00:02 -0400

    Create SimulationMethod.java

---
+++ Google Interview Prep/Data Structures/arrays/7. 2D Array/Spiral Matrix/SimulationMethod.java
@@ -0,0 +1,26 @@
+class SimulationMethod {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List ans = new ArrayList();
+        if (matrix.length == 0) return ans;
+        int R = matrix.length, C = matrix[0].length;
+        boolean[][] seen = new boolean[R][C];
+        int[] dr = {0, 1, 0, -1};
+        int[] dc = {1, 0, -1, 0};
+        int r = 0, c = 0, di = 0;
+        for (int i = 0; i < R * C; i++) {
+            ans.add(matrix[r][c]);
+            seen[r][c] = true;
+            int cr = r + dr[di];
+            int cc = c + dc[di];
+            if (0 <= cr && cr < R && 0 <= cc && cc < C && !seen[cr][cc]){
+                r = cr;
+                c = cc;
+            } else {
+                di = (di + 1) % 4;
+                r += dr[di];
+                c += dc[di];
+            }
+        }
+        return ans;
+    }
+}
