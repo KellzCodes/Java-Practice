6b0ecf9d074db6f9ff3353146ebff110fe29fcd6
Kelli Davis <keldavis5@yahoo.com>
2021-06-23 22:32:48 -0400

    Create Manipulation.java

---
+++ Google Interview Prep/Data Structures/strings/Add Binary/Manipulation.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+class Manipulation {
+  public String addBinary(String a, String b) {
+    BigInteger x = new BigInteger(a, 2);
+    BigInteger y = new BigInteger(b, 2);
+    BigInteger zero = new BigInteger("0", 2);
+    BigInteger carry, answer;
+    while (y.compareTo(zero) != 0) {
+      answer = x.xor(y);
+      carry = x.and(y).shiftLeft(1);
+      x = answer;
+      y = carry;
+    }
+    return x.toString(2);
+  }
+}
