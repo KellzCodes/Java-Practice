0abdd6703fc4d1fd3c358cae6a53c6b84f6ef7b8
Kelli Davis <keldavis5@yahoo.com>
2021-06-23 20:00:44 -0400

    Create LayerMethod.java

---
+++ Google Interview Prep/Data Structures/arrays/7. 2D Array/Spiral Matrix/LayerMethod.java
@@ -0,0 +1,22 @@
+class LayerMethod {
+    public List < Integer > spiralOrder(int[][] matrix) {
+        List ans = new ArrayList();
+        if (matrix.length == 0)
+            return ans;
+        int r1 = 0, r2 = matrix.length - 1;
+        int c1 = 0, c2 = matrix[0].length - 1;
+        while (r1 <= r2 && c1 <= c2) {
+            for (int c = c1; c <= c2; c++) ans.add(matrix[r1][c]);
+            for (int r = r1 + 1; r <= r2; r++) ans.add(matrix[r][c2]);
+            if (r1 < r2 && c1 < c2) {
+                for (int c = c2 - 1; c > c1; c--) ans.add(matrix[r2][c]);
+                for (int r = r2; r > r1; r--) ans.add(matrix[r][c1]);
+            }
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
+        return ans;
+    }
+}
