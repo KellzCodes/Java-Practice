804da1a5e05d12f95a9a643c728fd680246f206c
Kelli Davis <keldavis5@yahoo.com>
2021-06-26 23:51:20 -0400

    Create ArrayPartition1.java

---
+++ Google Interview Prep/Data Structures/arrays/8. Practice/Array Partition I/ArrayPartition1.java
@@ -0,0 +1,24 @@
+public class ArrayPartition1 {
+    public static void main (String args[]){
+        int[] nums = {1,4,3,2};
+        System.out.println(arrayPairSum(nums));
+    }
+    public static int arrayPairSum(int[] nums) {
+        int[] exist = new int[20001];
+        for (int i = 0; i < nums.length; i++) {
+            exist[nums[i] + 10000]++;
+        }
+        int sum = 0;
+        boolean odd = true;
+        for (int i = 0; i < exist.length; i++) {
+            while (exist[i] > 0) {
+                if (odd) {
+                    sum += i - 10000;
+                }
+                odd = !odd;
+                exist[i]--;
+            }
+        }
+        return sum;
+    }
+}
