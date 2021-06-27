d7feb6a37cf8297724c0539f558ead1c9ec6569e
Kelli Davis <keldavis5@yahoo.com>
2021-06-26 23:44:16 -0400

    Create TwoPointers.java

---
+++ Google Interview Prep/Data Structures/strings/Reverse String/TwoPointers.java
@@ -0,0 +1,10 @@
+class TwoPointers {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+            char tmp = s[left];
+            s[left++] = s[right];
+            s[right--] = tmp;
+        }
+    }
+}
