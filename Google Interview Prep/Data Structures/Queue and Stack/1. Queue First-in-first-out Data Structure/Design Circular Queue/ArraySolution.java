57a0031e614c39ff70979ad498182fac1a5f7694
Kelli Davis <keldavis5@yahoo.com>
2021-06-27 23:38:38 -0400

    Create ArraySolution.java

---
+++ Google Interview Prep/Data Structures/Queue and Stack/1. Queue First-in-first-out Data Structure/Design Circular Queue/ArraySolution.java
@@ -0,0 +1,58 @@
+class ArraySolution {
+
+  private int[] queue;
+  private int headIndex;
+  private int count;
+  private int capacity;
+
+  /** Initialize your data structure here. Set the size of the queue to be k. */
+  public MyCircularQueue(int k) {
+    this.capacity = k;
+    this.queue = new int[k];
+    this.headIndex = 0;
+    this.count = 0;
+  }
+
+  /** Insert an element into the circular queue. Return true if the operation is successful. */
+  public boolean enQueue(int value) {
+    if (this.count == this.capacity)
+      return false;
+    this.queue[(this.headIndex + this.count) % this.capacity] = value;
+    this.count += 1;
+    return true;
+  }
+
+  /** Delete an element from the circular queue. Return true if the operation is successful. */
+  public boolean deQueue() {
+    if (this.count == 0)
+      return false;
+    this.headIndex = (this.headIndex + 1) % this.capacity;
+    this.count -= 1;
+    return true;
+  }
+
+  /** Get the front item from the queue. */
+  public int Front() {
+    if (this.count == 0)
+      return -1;
+    return this.queue[this.headIndex];
+  }
+
+  /** Get the last item from the queue. */
+  public int Rear() {
+    if (this.count == 0)
+      return -1;
+    int tailIndex = (this.headIndex + this.count - 1) % this.capacity;
+    return this.queue[tailIndex];
+  }
+
+  /** Checks whether the circular queue is empty or not. */
+  public boolean isEmpty() {
+    return (this.count == 0);
+  }
+
+  /** Checks whether the circular queue is full or not. */
+  public boolean isFull() {
+    return (this.count == this.capacity);
+  }
+}
