b349af54f118cbc102186625c74e2351d9c36383
Kelli Davis <keldavis5@yahoo.com>
2021-06-27 21:40:09 -0400

    Create SimpleSolution.java

---
+++ Google Interview Prep/Data Structures/strings/Reverse Words in a String III/SimpleSolution.java
@@ -0,0 +1,9 @@
+public class SimpleSolution {
+    public String reverseWords(String s) {
+        String words[] = s.split(" ");
+        StringBuilder res=new StringBuilder();
+        for (String word: words)
+            res.append(new StringBuffer(word).reverse().toString() + " ");
+        return res.toString().trim();
+    }
+}
