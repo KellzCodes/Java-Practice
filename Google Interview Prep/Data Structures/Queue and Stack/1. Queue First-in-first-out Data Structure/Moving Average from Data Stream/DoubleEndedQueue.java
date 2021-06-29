f811feb1fba26b963779cb22274c405d90588dcd
Kelli Davis <keldavis5@yahoo.com>
2021-06-29 00:10:32 -0400

    Create DoubleEndedQueue.java

---
+++ Google Interview Prep/Data Structures/Queue and Stack/1. Queue First-in-first-out Data Structure/Moving Average from Data Stream/DoubleEndedQueue.java
@@ -0,0 +1,19 @@
+class DoubleEndedQueue {
+  int size, windowSum = 0, count = 0;
+  Deque queue = new ArrayDeque<Integer>();
+
+  public MovingAverage(int size) {
+    this.size = size;
+  }
+
+  public double next(int val) {
+    ++count;
+    // calculate the new sum by shifting the window
+    queue.add(val);
+    int tail = count > size ? (int)queue.poll() : 0;
+
+    windowSum = windowSum - tail + val;
+
+    return windowSum * 1.0 / Math.min(size, count);
+  }
+}
