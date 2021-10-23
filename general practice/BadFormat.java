28e55c2e121f008f7ab8480fa78b029448b6f400
Kelli Davis <keldavis5@yahoo.com>
2021-10-22 21:03:05 -0400

    Delete general practice directory

--- general practice/BadFormat.java
+++
@@ -1,9 +0,0 @@
-//********************************************************************
-// BadFormat.java
-// Author: Kelli Davis
-//
-// Demonstrates a poorly formatted, though valid, program.
-//********************************************************************
-public class BadFormat { public static void main(String[]args){
-    System.out.println("A quote by Dr. Frankenfurter:");
-    System.out.println("I see you shiver with antici......pation!");}}
