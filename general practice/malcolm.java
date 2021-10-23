17fa43012c4322b25dc7e60b959981fcdba9a5ca
Kelli Davis <keldavis5@yahoo.com>
2021-10-22 21:08:36 -0400

    Prints a Malcolm X quote.
    Demonstrates the basic structure of a Java application

---
+++ general practice/malcolm.java
@@ -0,0 +1,16 @@
+//********************************************************************
+// Malcolm.java     Author: Kelli Davis
+//
+// Demonstrates the basic structure of a Java application.
+//********************************************************************
+public class Malcolm
+{
+ //-----------------------------------------------------------------
+ // Prints a Malcolm X quote.
+ //-----------------------------------------------------------------
+ public static void main(String[] args)
+ {
+ System.out.println("A quote by Malcolm X:");
+ System.out.println("A man who stands for nothing will fall for anything.");
+ }
+}
