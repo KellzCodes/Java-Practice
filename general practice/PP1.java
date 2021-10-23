a9cd5b04d747cdc8e900a447bdd189ecadd5d909
Kelli Davis <keldavis5@yahoo.com>
2021-10-22 21:34:38 -0400

    Write a program that prints on separate lines
    Write a program that prints on separate lines, the name of your favorite book, its author, category, price, and total number of pages. Label each piece of information in the output.

---
+++ general practice/PP1.java
@@ -0,0 +1,15 @@
+public class PP1 {
+    /**
+     * Write a program that prints on separate lines, the name of your
+     * favorite book, its author, category, price, and total number of
+     * pages. Label each piece of information in the output.
+     * */
+
+    public static void main(String[] args){
+        System.out.println("Name of fav book: Grit: The Power of Passion and Perseverance\n" +
+                "author: Angela Duckworth\n" +
+                "category: Self Help\n" +
+                "price: 1 audible credit\n" +
+                "total pages: 368");
+    }
+}
