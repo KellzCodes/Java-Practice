795ac5b3b9b7de5366c341f1ca4310b94b3ccffb
Kelli Davis <keldavis5@yahoo.com>
2021-10-22 21:45:25 -0400

    Write a program that prints a list
    Write a program that prints a list of two or three games that you enjoy. Print the name and number of players in the game.

---
+++ general practice/PP2.java
@@ -0,0 +1,11 @@
+public class PP2 {
+    /**
+     * Write a program that prints a list of two or three games that you
+     * enjoy. Print the name and number of players in the game.
+     * */
+
+    public static void main(String[] args){
+        System.out.println("Red Dead Redemption 2: Single player story / Mass online platform\n" +
+                "Call of Duty Mobile: Single player per device / Mass online platform");
+    }
+}
