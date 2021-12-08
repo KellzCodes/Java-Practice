f6e2b8ee8488919b94f7e2204dbd120c1f9f5fa6
Kelli Davis <keldavis5@yahoo.com>
2021-12-08 14:06:50 -0500

    prints the outline of a house using tilde (~) characters

---
+++ textbook work/java software solutions/intro/PP2.java
@@ -0,0 +1,17 @@
+public class PP2 {
+    /**
+     * Write a program that prints the outline of a house using tilde (~)
+     * characters.
+     * */
+
+    public static void main(String[] args){
+        System.out.println("    ~\n" +
+                "   ~ ~\n" +
+                "  ~   ~\n" +
+                " ~~~~~~~\n" +
+                " ~     ~\n" +
+                " ~     ~\n" +
+                " ~     ~\n" +
+                "~~~~~~~~~");
+    }
+}
