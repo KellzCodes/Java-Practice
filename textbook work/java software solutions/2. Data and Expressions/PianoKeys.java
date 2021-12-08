92fd6f3c3dc64cce627883b03e36432b10d0d5b1
Kelli Davis <keldavis5@yahoo.com>
2021-12-08 15:33:26 -0500

    shows declaration, initialization, and use of an integer variable
    Prints the number of keys on a piano.

---
+++ textbook work/java software solutions/2. Data and Expressions/PianoKeys.java
@@ -0,0 +1,18 @@
+//********************************************************************
+// PianoKeys.java Author: Lewis/Loftus
+//
+// Demonstrates the declaration, initialization, and use of an
+// integer variable.
+//********************************************************************
+
+public class PianoKeys
+{
+  //-----------------------------------------------------------------
+  // Prints the number of keys on a piano.
+  //-----------------------------------------------------------------
+  public static void main(String[] args)
+  {
+    int keys = 88;
+    System.out.println("A piano has " + keys + " keys.");
+  }
+}
