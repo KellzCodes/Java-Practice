60b7b71afd71ab933794a8ef391fa518ef9e92de
Kelli Davis <keldavis5@yahoo.com>
2021-12-08 13:11:57 -0500

    Create Obama.java
    Demonstrates the basic structure of a Java application.
    Prints a presidential quote

---
+++ textbook work/java software solutions/Obama.java
@@ -0,0 +1,16 @@
+//********************************************************************
+// Obama.java Author: Kelli Davis
+//
+// Demonstrates the basic structure of a Java application.
+//********************************************************************
+public class Obama
+{
+     //-----------------------------------------------------------------
+     // Prints a presidential quote.
+     //-----------------------------------------------------------------
+     public static void main(String[] args)
+     {
+         System.out.println("A quote by Barack Obama:");
+         System.out.println("I think when you spread the wealth around it's good for everybody.");
+     }
+}
