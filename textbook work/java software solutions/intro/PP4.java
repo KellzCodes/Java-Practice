d0be1d5647b50a9529ca0eb203560a585fc0d73b
Kelli Davis <keldavis5@yahoo.com>
2021-12-08 14:39:44 -0500

    displays the word “HI” in large block letters

---
+++ textbook work/java software solutions/intro/PP4.java
@@ -0,0 +1,18 @@
+public class Practice {
+    /**
+     * Write a program that displays the word “HI” in large block
+     *  letters. Make each large letter out of the other character
+     * */
+
+    public static void main(String[] args){
+        System.out.println("III     III     HHHHHHHHHHH\n" +
+                "III     III          H\n" +
+                "III     III          H\n" +
+                "III     III          H\n" +
+                "IIIIIIIIIII          H\n" +
+                "III     III          H\n" +
+                "III     III          H\n" +
+                "III     III          H\n" +
+                "III     III     HHHHHHHHHHH\n");
+    }
+}
