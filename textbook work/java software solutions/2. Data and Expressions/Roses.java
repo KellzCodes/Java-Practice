de94eaeedb79aa616e5557eba9d7e4cfeee885f9
Kelli Davis <keldavis5@yahoo.com>
2021-12-08 15:25:51 -0500

    Demonstrates the use of escape sequences.

---
+++ textbook work/java software solutions/2. Data and Expressions/Roses.java
@@ -0,0 +1,18 @@
+//********************************************************************
+// Roses.java Author: Lewis/Loftus
+//
+// Demonstrates the use of escape sequences.
+//********************************************************************
+public class Roses
+{
+     //-----------------------------------------------------------------
+     // Prints a poem (of sorts) on multiple lines.
+     //-----------------------------------------------------------------
+     public static void main(String[] args)
+     {
+         System.out.println("Roses are red,\n\tViolets are blue,\n" +
+         "Sugar is sweet,\n\tBut I have \"commitment issues\",\n\t" +
+         "So I'd rather just be friends\n\tAt this point in our " +
+         "relationship.");
+     }
+}
