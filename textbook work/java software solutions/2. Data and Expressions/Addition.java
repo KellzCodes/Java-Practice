f5142eecacad72d9d46f795bd99309f3b3295431
Kelli Davis <keldavis5@yahoo.com>
2021-12-08 14:54:51 -0500

    Shows difference between addition and concatenation
    Concatenates and adds two numbers and prints the results.

---
+++ textbook work/java software solutions/2. Data and Expressions/Addition.java
@@ -0,0 +1,17 @@
+//********************************************************************
+// Addition.java Author: Lewis/Loftus
+//
+// Demonstrates the difference between the addition and string
+// concatenation operators.
+//********************************************************************
+public class Addition
+{
+ //-----------------------------------------------------------------
+ // Concatenates and adds two numbers and prints the results.
+ //-----------------------------------------------------------------
+ public static void main(String[] args)
+ {
+     System.out.println("24 and 45 concatenated: " + 24 + 45);
+     System.out.println("24 and 45 added: " + (24 + 45));
+ }
+}
